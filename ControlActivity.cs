893585fb10af08345769b5fb1bc6f3986bfe21f1
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2021-12-12 09:03:57 +0300

    Update ControlActivity.cs

--- ControlActivity.cs
+++ ControlActivity.cs
@@ -43,7 +43,7 @@ namespace XA_SQLite_Lectures
             {
                 Id.Text = Convert.ToString(lecture.Id);
                 Code.Text = lecture.CourseCode;
-                Course.Text = lecture.CourseCode;
+                Course.Text = lecture.CourseName;
                 Teacher.Text = lecture.TeacherName;
             }
 
